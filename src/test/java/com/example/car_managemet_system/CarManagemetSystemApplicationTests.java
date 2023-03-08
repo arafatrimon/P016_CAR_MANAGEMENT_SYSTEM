3dee15ba5354359c1e6c14fb4884e7ddf057ccfa
arafat-rimon <rimonarafat2020@gmail.com>
2023-03-08 23:04:58 +0600

    first commit

---
+++ src/test/java/com/example/car_managemet_system/CarManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_managemet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagemetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
